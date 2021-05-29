b1efe93b1b5bc0898721f0f8e916b399505a041c
Alex Bryant <alexanderpbryant@gmail.com>
2021-05-28 19:41:33 -0500

    updated form event listeners

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ console.log("test 1");
 
 // storage
 let tastStorage = [];
-let listStorage = ["Reminders"];
+let listStorage = ["Reminders", "Work Stuff"];
 
 // ------------task factory-----------------------------
 
@@ -20,7 +20,10 @@ const task = (title, list, notes, dueDate, listPriority, allPriority) => {
 }
 
 
-
+function dotest (e) {
+    e.preventDefault();
+    console.log('submited!!!!');
+}
 
 
 // -------------- dom listeners -----------------
@@ -31,39 +34,47 @@ const sortBySelector = document.getElementById("sortBySelector");
 const taskFormContainer = document.querySelector(".taskFormContainer");
 const taskForm = document.querySelector(".taskForm");
 const taskText = document.getElementById("taskText");
+const listSelector = document.getElementById('listSelector');
+
 
 addListButton.addEventListener("click", createNewList);
 newTaskButton.addEventListener("click", createTaskForm);
 sortBySelector.addEventListener("change", () => {console.log(sortBySelector.value)});
-//taskForm.addEventListener('submit', taskFormSubmit);
+taskForm.addEventListener('submit', dotest); // make this function taskFormSubmit
+window.addEventListener('keydown', function(e) {
+  if (e.key === "Escape") {
+    resetTaskForm();
+  }
+})
+window.addEventListener("keydown", function(e) {
+    if (taskFormContainer.style.display == "block" && e.key === "Enter") {
+        console.log("this would submit form");
+        resetTaskForm();
+    }
+})
+
+
+
+
 // When the user clicks anywhere outside of the taskform, 
 //submit if title is not empty but either way close it
-window.onclick = function(event) { //this logic needs work
-  if (event.target != taskFormContainer && event.target != newTaskButton) {
-        if(taskText.value != "") {
-            //create task object
-            //push object to array
-            //clear form
-            //hide form
-            resetTaskForm();
-        } else {
-            // clear form
-            //hide form
-            resetTaskForm();
-        }
-    }
-    console.log("not the form");
-}
-//if you hit escape it closes form and clears form.
-// window.addEventListener('keydown', function(e) {
-//   if (e.key === "Escape") {
-//     formPopup.style.display = "none";
-//   }
-// })
-
-//when you list awawy from form if title is filled out it submits and clears and hides form
-
-
+// window.onclick = function(event) { //this logic needs work
+//   if (event.target != taskFormContainer || event.target != newTaskButton) {
+//     console.log(event.target);
+//     if(taskText.value != "") {
+//             create task object
+//             push object to array
+//             clear form
+//             hide form
+//            resetTaskForm();
+//         } else {
+//             clear form
+//             hide form
+//            resetTaskForm();
+//         }
+//     }
+//     console.log("not the form");
+// }
 
 //--------------- dom editors -----------------------
 
@@ -76,9 +87,21 @@ function createNewList () {
     console.log ("new list yay");
 }
 function resetTaskForm () {
-    //add clearing the form to this
+    taskForm.reset();
     taskFormContainer.style.display = "none";
 }
+function renderLists () {
+    listStorage.forEach(listIndex => {
+        var listOption = document.createElement('option');
+        listOption.value = `${listIndex}`;
+        listOption.innerHTML = `${listIndex}`;
+        listSelector.appendChild(listOption);
+    })
+}
+
+//------------------ 
+renderLists();
+
 
 // - list creation tab
 // - ability to change priority
